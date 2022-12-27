44e55e3cbd93a44d96fd9e6f052d0e624659362f
atharvatekawade <64606981+atharvatekawade@users.noreply.github.com>
2022-12-27 15:13:09 +0530

    Add files via upload

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo " BUILD START"
-pip install -r requirements.txt
-python3 manage.py collectstatic  --noinput --clear
+python3.7  -m pip install -r requirements.txt
+python3.7 manage.py collectstatic  --noinput --clear
 echo " BUILD END"
